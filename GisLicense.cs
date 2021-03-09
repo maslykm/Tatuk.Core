25a83b135ed884dbf60fc594c7fe203e786e29b9
Mirosław Masłyk <mmaslyk@unisoft.com.pl>
2021-03-09 11:51:26 +0100

    Init

---
+++ GisLicense.cs
@@ -0,0 +1,13 @@
+// MASLYKM#WP.PL-449432-7IA3R4SX-84AB2CBC
+namespace TatukGIS.NDK
+{
+  class GisLicense
+  {
+    public static void Initialize()
+    {
+      TGIS_LicenseManager.SetLicense(
+        "amFHhwJB091nS+dby7oZrQnGlSlfuYAhotBEmr8K5f/hFRrzD3lQfhTN53Zx8Ug6QJLRbE9SbXMAhzTlJnz+zJz5I7jjtEZpbHQBXwW2I5atJqvM/xhRrFB7famL48IrYcw61/OhGYhZIP6C5Po+xh3MMUwm0t6YHi3nAzNou3B8Qg+Ntr2PlG4X4DSzQfD4yrpsOApTllDZL81ZeKorSBgdZIEtSX3INmLKgibiaXw="
+      );
+    }
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using TatukGIS.NDK;
+using TatukGIS.NDK.Common;
+
+namespace SimpleMap
+{
+    public class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            GisLicense.Initialize();
+
+            using TGIS_ViewerBmp viewer = new TGIS_ViewerBmp();
+        }
+    }
+}
